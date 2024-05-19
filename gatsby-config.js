1a7a9d4739595571bae92b04d360348aba8fdb9b
Rangho Studio <161280847+Rangho27@users.noreply.github.com>
2024-05-19 22:28:48 +0900

    Initial commit

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `cloudtype-gatsby-template`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
